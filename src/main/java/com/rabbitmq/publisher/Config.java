3067171ebb9933e7e26a3adae04bda5299a83c77
wyoung163 <20201014@sungshin.ac.kr>
2023-08-22 18:21:14 +0900

    Initial commit: Publisher

---
+++ src/main/java/com/rabbitmq/publisher/Config.java
@@ -0,0 +1,41 @@
+package com.rabbitmq.publisher;
+
+import ch.qos.logback.classic.pattern.MessageConverter;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+
+import java.sql.Connection;
+
+public class Config {
+    private static final String EXCHANGE = "forest.exchange";
+    private static final String QUEUE = "forest.queue";
+    private static final String ROUTING_KEY = "forest.key.#";
+
+    @Bean
+    TopicExchange exchange() {
+        return new TopicExchange(EXCHANGE);
+    }
+
+    @Bean
+    Queue queue() {
+        return new Queue(QUEUE);
+    }
+
+    @Bean
+    Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
+    }
+
+    @Bean
+    RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, MessageConverter messageConverter){
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
+        return rabbitTemplate;
+    }
+}
---
+++ src/main/java/com/rabbitmq/publisher/Controller.java
@@ -0,0 +1,22 @@
+package com.rabbitmq.publisher;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    private static final String EXCHANGE = "forest.exchange";
+
+    @Autowired
+    RabbitTemplate rabbitTemplate;
+
+    @GetMapping("/forest/queue")
+    public String forestPublish() {
+        rabbitTemplate.convertAndSend(EXCHANGE, "forest.key.#", "RabbitMQ + Springboot = Success");
+        return "message sent";
+//        rabbitTemplate.convertAndSend(EXCHANGE, "forest.key.#", "RabbitMQ and SpringBoot Test");
+//        return "message sending";
+    }
+}
