2aaa5595669442fc631e3e6a8988c2c9737e4b9c
wyoung163 <20201014@sungshin.ac.kr>
2023-08-23 00:07:16 +0900

    불필요한 파라미터 제거

--- src/main/java/com/rabbitmq/publisher/Config.java
+++ src/main/java/com/rabbitmq/publisher/Config.java
@@ -1,6 +1,5 @@
 package com.rabbitmq.publisher;
 
-import ch.qos.logback.classic.pattern.MessageConverter;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
@@ -8,10 +7,11 @@ import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-import java.sql.Connection;
-
+@Configuration
 public class Config {
     private static final String EXCHANGE = "forest.exchange";
     private static final String QUEUE = "forest.queue";
@@ -33,7 +33,7 @@ public class Config {
     }
 
     @Bean
-    RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, MessageConverter messageConverter){
+    RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory){
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
         return rabbitTemplate;
